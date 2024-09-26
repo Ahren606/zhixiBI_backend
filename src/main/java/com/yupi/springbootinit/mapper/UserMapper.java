e3dc49d2df7a7d2fbd292a3a544cbc8731b81e8a
Ahren606 <2418764378@qq.com>
2024-09-26 23:49:57 +0800

    update：初始化项目框架 -创建 user和chart -编写 chart 接口参数类 -重构 controller

---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.User;
+
+/**
+* @author 24187
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-09-26 17:05:11
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
